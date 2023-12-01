b961060c175b382ac9d502c16c4567d837f3f3f2
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-01 16:51:40 -0300

    feat: add DiscountCalculatorTest

---
+++ DiscountCalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class DiscountCalculatorTest {
+    
+    public function ShouldApplyWhenValueIsAboveTheMinimum() {
+        $discountCalculator = new DiscountCalculator();
+        $totalValue = 130;
+        $totalWithDiscount = $discountCalculator->apply($totalValue);
+
+        $expectedValue = 110;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+    }
+
+    public function assertEquals($expectedValue, $actualValue) {
+        if ($expectedValue !== $actualValue) {
+            $message = 'Expected: ' . $expectedValue . ' but got: ' . $actualValue;
+            throw new \Exception($message);
+        }
+
+        echo "Test passed! \n";
+    }
+}
